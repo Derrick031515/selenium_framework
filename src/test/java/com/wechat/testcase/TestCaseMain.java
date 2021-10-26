c03f132ce8575714e9c31239f0d4532711424cf9
Derrick <1058183687@qq.com>
2021-10-26 10:16:57 +0800

    Modify some files

--- src/test/java/com/wechat/testcase/TestCaseMain.java
+++ src/test/java/com/wechat/testcase/TestCaseMain.java
@@ -1,30 +1,12 @@
 package com.wechat.testcase;
 
-import com.wechat.pageobject.CeshirenPOCodeSearchPage;
-import org.junit.platform.launcher.Launcher;
-import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestExecutionListener;
-import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
-import org.junit.platform.launcher.core.LauncherFactory;
-import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import com.wechat.pageobject.CeshirenPOCodeSearchPageTest;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
 
-import static org.junit.platform.engine.discovery.ClassNameFilter.excludeClassNamePatterns;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
-
+@RunWith(JUnitPlatform.class)
+@SelectClasses({CeshirenPOCodeSearchPageTest.class})
 public class TestCaseMain {
-    public static void main(String[] args) {
-        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-                .selectors(
-                        selectPackage("com.wechat.pageobject"),
-                        selectClass(CeshirenPOCodeSearchPage.class)
-//                        selectMethod("examples.packageA.ClassATest#testCaseA"),
-                ).filters().build();
-        Launcher launcher = LauncherFactory.create();
 
-        /*// Register a listener of your choice
-        TestExecutionListener listener = new SummaryGeneratingListener();
-        launcher.registerTestExecutionListeners(listener);*/
-
-        launcher.execute(request);
-    }
 }
